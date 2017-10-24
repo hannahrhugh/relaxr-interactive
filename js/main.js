25641100482dc6932a3bbf369609113dc0b91fac
hannahrhugh <30412121+hannahrhugh@users.noreply.github.com>
2017-10-24 21:07:44 +0100

    JS additions

--- js/main.js
+++ js/main.js
@@ -16,8 +16,20 @@ $('.readmore').find('a').on('click', function(event){
 // hide read more
 
 // if read less is selected <p> must slide up and read more must show
-$('.readless hide').find('a')
+$('.readless').find('a').on('click', function(event){
+	event.preventDefault();
+	$('#show-this-on-click').slideUp() ;
+	$('.readmore').show() ;
+	$('.readless').hide() ;
+})
 
-// 
+// If user clicks learn more in the sidebar, span inside p must slide down
+
+$('.learnmore').on('click', function(event){
+	event.preventDefault();
+	$('#learnmoretext').slideDown();
+	$('.learnmore').hide();
+
+})
 
 });
